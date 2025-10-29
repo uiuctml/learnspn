4a1a5a63eb8737b494f334ef66fec474537b4512
Simon Yu <jundayu2@illinois.edu>
2025-10-29 17:44:37 -0500

    Add random script

---
+++ scripts/random/random.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+@file   random.py
+@author Simon Yu
+@date   10/29/2025
+@brief  Script for random PCs.
+"""
+
+def main():
+    print("Hello World")
+
+    return
+
+if __name__ == "__main__":
+    main()
